9a7403c646c8723078ea71cb50568852bb3b3301
admin <admin>
2020-08-11 13:35:27 +0200

    Edited files

--- js/script.js
+++ js/script.js
@@ -37,9 +37,10 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
     };
     
     $scope.getViewProperties = function(){
+        $scope.metricsReady=false;
         $scope.callPythonDo({method: "get_metrics_and_goals"}).then(function(data){
         
-        $scope.metricsReady=false;
+        
         $scope.metricsList = data['metrics_and_goals'];
         
         $scope.metricsReady=true;
