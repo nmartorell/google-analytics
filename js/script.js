b84f9a8945a5b312ed64c251a41f1e34b0c65eb7
admin <admin>
2020-08-12 19:08:21 +0200

    Edited files

--- js/script.js
+++ js/script.js
@@ -31,9 +31,9 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
         $scope.callPythonDo({method: "get_account_summaries"}).then(function(data){
             $scope.account_summaries = data['account_summaries'];
             
-            /* Clear Web Property and Views dropdowns */
+            /* Clear Web Property and Views dropdowns 
             $scope.web_properties = null;
-            $scope.views = null;
+            $scope.views = null; */
         });
         
         /* TODO: deal with elements that need to be removed !!! */
