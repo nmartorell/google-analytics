839513ce538f44caf4b5ac2c582dad7b23933746
admin <admin>
2020-08-13 14:47:44 +0200

    Edited files

--- resource/dataset-helper.py
+++ resource/dataset-helper.py
@@ -19,9 +19,6 @@ def do(payload, config, plugin_config, inputs):
     if payload["method"] == "get_account_summaries":
         return get_account_summaries(config)
     
-    if payload["method"] == "get_views":
-        return get_views(config)
-    
     if payload["method"] == "get_view_properties":
         metrics_and_goals = get_metrics_and_goals(config)
         dimensions = get_dimensions(config)
