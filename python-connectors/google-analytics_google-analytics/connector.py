89a6ade0cc1cc67344588b4488967c3b7dcc3841
admin <admin>
2020-08-18 15:45:02 +0200

    Edited file 'python-connectors/google-analytics_google-analytics/connector.py'

--- python-connectors/google-analytics_google-analytics/connector.py
+++ python-connectors/google-analytics_google-analytics/connector.py
@@ -43,7 +43,7 @@ class MyConnector(Connector):
         self.service = ga_api.get_authenticated_google_analytics_service(api_name, api_version, scope, service_account_name) 
 
         # (2) Validate Query Target
-        assert self.config["view"], "Please select a Query Target (including an Account, Web Property and View)." 
+        assert self.config.get("view", None), "Please select a Query Target (including an Account, Web Property and View)." 
         
         # (3) Validate Query Parameters
         assert (len(self.config["metrics"]) >= 1) and (len(self.config["metrics"]) <= 10)
