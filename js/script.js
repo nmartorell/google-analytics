7379c34ba82aeed43e4d6e171322edc25833f624
admin <admin>
2020-08-18 21:49:36 +0200

    Edited file 'js/script.js'

--- js/script.js
+++ js/script.js
@@ -47,11 +47,10 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
     /* Function to retrieve Account Summaries dict */
     $scope.getAccountSummaries = function(){
         
-        /* Prevent from running when $scope.config.service_account is null 
+        /* Prevent from running when $scope.config.service_account is null */
         if ($scope.config.service_account == null) {
             return;
         };
-        */
         
         /* Clear View Property multiselect fields */
         clearViewProperties();
