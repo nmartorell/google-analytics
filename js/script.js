7371afb04385a167f28e4e8a36b1f7bcd8f813a7
admin <admin>
2020-08-18 22:35:53 +0200

    Edited file 'js/script.js'

--- js/script.js
+++ js/script.js
@@ -162,7 +162,7 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
         
         /* TODO: add validation for failure */
         DataikuAPI.plugins.listAccessiblePresets(pluginId, projectKey, parameterSetId).success(function(data){
-            $scope.presets = angular.toJson(data.presets.filter(p => p.usable));
+            $scope.presets = data.presets.filter(p => p.usable);
         });
         
         /* UI features enable/disable at start */
