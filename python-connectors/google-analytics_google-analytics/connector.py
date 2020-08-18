5e3ef50651d65b304dd28123f58f57ee4b39eebf
admin <admin>
2020-08-18 15:30:13 +0200

    Edited file 'python-connectors/google-analytics_google-analytics/connector.py'

--- python-connectors/google-analytics_google-analytics/connector.py
+++ python-connectors/google-analytics_google-analytics/connector.py
@@ -28,22 +28,22 @@ class MyConnector(Connector):
         Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
 
         # (1) Get a service object for the Google Analytics Reporting API (V4)
-        
-        # (1.1) API variables
+        # (1.1) API configuration
         scope = ['https://www.googleapis.com/auth/analytics.readonly']
         api_name = 'analyticsreporting'
         api_version = 'v4'
     
         # (1.2) Retrieve Service Account name
         try:
-            service_account_cname = self.config["service_account"]["name"]
+            service_account_name = self.config["service_account"]["name"]
         except KeyError as e:
             raise KeyError("No Google Analytics Service Account has been selected. If none are available, please contact your DSS Administrator.") from e
     
-        # (1.3) Get service object
-        
-        self.service = ga_api.get_service(api_name, api_version, scope, service_account_credentials) 
+        # (1.3) Get Google Analytics service object
+        self.service = ga_api.get_authenticated_google_analytics_service(api_name, api_version, scope, service_account_name) 
 
+        # (2) 
+        
     def get_read_schema(self):
         """
         Returns the schema that this connector generates when returning rows.
