9cafaf039d741120363a1bc5d311a8ee84646f50
admin <admin>
2020-08-19 15:59:11 +0200

    Edited file 'js/script.js'

--- js/script.js
+++ js/script.js
@@ -90,6 +90,7 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
         });
         
         /* Retrieve currently configured service accounts */
+        $scope.presets = null;''
         DataikuAPI.plugins.listAccessiblePresets(pluginId, projectKey, parameterSetId).success(function(data){
             $scope.presets = data.presets.filter(p => p.usable);
         }); 
