38ed71b2b11bababe034bdb372f39d074deb1b6c
admin <admin>
2020-08-21 14:37:17 +0200

    Edited file 'python-connectors/google-analytics_google-analytics/connector.py'

--- python-connectors/google-analytics_google-analytics/connector.py
+++ python-connectors/google-analytics_google-analytics/connector.py
@@ -60,6 +60,10 @@ class GoogleAnalyticsConnector(Connector):
         assert len(self.dimensions) <= 9, "More than 9 Google Analytics \"Dimensions\" have been selected; please select a maximum of 9."
         assert len(self.segments) <= 4, "More than 4 Google Analytics \"Segments\" have been selected; please select a maximum of 4."
     
+        # A ga:segments dimension is required if segments are defined (see: https://developers.google.com/analytics/devguides/reporting/core/v4/basics#segments)
+        if len(self.segments) > 0:
+            self.dimensions.append({"name":"Segment", "id":"ga:segment"}) 
+    
         # (4) Query Dates
         self.start_date = self.config.get("start_date", None)
         self.end_date = self.config.get("end_date", None)
