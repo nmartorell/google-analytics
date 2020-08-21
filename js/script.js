d2f7dabf5cc54fdf705dd1be79dce3a24a07cd20
admin <admin>
2020-08-21 15:30:41 +0200

    Edited file 'js/script.js'

--- js/script.js
+++ js/script.js
@@ -90,6 +90,7 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
         /* Validate here */
         
         /* Project Key */
+        $scope.config.project_key=""
         $scope.callPythonDo({method: "get_project_key"}).then(function(data){
             $scope.config.project_key = data.project_key;
         });
