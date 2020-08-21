dab4f508f0f7112ab12a354387168cf7614e4215
admin <admin>
2020-08-21 15:54:51 +0200

    Edited file 'js/script.js'

--- js/script.js
+++ js/script.js
@@ -84,7 +84,7 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
     var init = function(){        
         
         /* Set Plugin and Parameter Set IDs */
-        $scope.config.plugin_id = "google-analyticss";
+        $scope.config.plugin_id = "google-analytics";
         $scope.config.service_account_preset_id = "google-service-accounts";
         
         $scope.callPythonDo({method: "validate_plugin_and_preset_ids"})
