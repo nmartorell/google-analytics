9e9b2219a110badad3d9b4de33cbb64ab725cf1c
admin <admin>
2020-08-21 15:44:43 +0200

    Edited file 'resource/dataset-helper.py'

--- resource/dataset-helper.py
+++ resource/dataset-helper.py
@@ -56,9 +56,7 @@ def validate_plugin_and_preset_ids(plugin_id, service_account_preset_id):
     # Retrieve plugin
     client = dataiku.api_client()
     plugin = client.get_plugin(plugin_id)
-    
-    
-    
+
     settings = plugin.get_settings()
     
         
