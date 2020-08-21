7e5d1f265d4a74f41c2a5c3524c8262b38ff4e85
admin <admin>
2020-08-21 15:39:11 +0200

    Edited file 'resource/dataset-helper.py'

--- resource/dataset-helper.py
+++ resource/dataset-helper.py
@@ -46,7 +46,6 @@ def do(payload, config, plugin_config, inputs):
         raise ValueError("I forgot to define a python helper function... whoops! This is a bug.") 
 
     
-    
 def validate_plugin_and_preset_ids(plugin_id, service_account_preset_id):
     
         
