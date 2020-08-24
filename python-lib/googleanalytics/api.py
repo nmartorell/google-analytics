1229e11fe8e4b0f56dfbb45ca228334fbbaaf0f1
admin <admin>
2020-08-24 17:09:53 +0200

    Edited file 'python-lib/googleanalytics/api.py'

--- python-lib/googleanalytics/api.py
+++ python-lib/googleanalytics/api.py
@@ -29,15 +29,15 @@ def get_authenticated_service(api_name, api_version, scope, plugin_id, service_a
         raise Exception("Service Account Preset not found, most likely due to it having been deleted. Select a different Service Account under the dataset settings, " + \
                         "or contact yout DSS Administrator if none are available.")
     
-    # Decrypt preset and retrieve Google Analytics Service
-    try: 
-        service_account_credentials_str = subprocess.Popen("$DIP_HOME/bin/dku decrypt-password " + str(service_account_credentials_encrypted), 
-                                                           shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout.read()
-        
+    # Decrypt preset account key
+    service_account_credentials_str = subprocess.Popen("$DIP_HOME/bin/dku decrypt-password " + str(service_account_credentials_encrypted), 
+                                                       shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout.read()
+    
+    # Retreieve Google Analytics service
+    try:
         service_account_credentials_json = ast.literal_eval(service_account_credentials_str)
         credentials = ServiceAccountCredentials.from_json_keyfile_dict(service_account_credentials_json, scope)
-        service = build(api_name, api_version, credentials=credentials)
-    
+        service = build(api_name, api_version, credentials=credentials)    
     except:
         raise Exception("Failed to retreive authenticated Google Analytics API Service. Caused by an invalid Serivce Account Secret key. " + \
                         "See stacktrace for more details, and contact your DSS Administrator.")
