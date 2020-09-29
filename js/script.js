165c13d51805bf070ceffedf0be3defb6c77b5f0
admin <admin>
2020-09-29 16:09:41 -0400

    Edited files

--- js/script.js
+++ js/script.js
@@ -35,15 +35,11 @@ app.controller('googleAnalyticsDatasetController', function($scope, DataikuAPI) 
         $scope.callPythonDo({method: "get_service_account_credentials"}).then(function(data){
             $scope.config.service_account_credentials = data['service_account_credentials'];
             
-            
             /* Call Google Analytics API to retrieve Account Summaries */
-        $scope.callPythonDo({method: "get_account_summaries"}).then(function(data){
-            $scope.config.account_summaries_list = data['account_summaries'];
+            $scope.callPythonDo({method: "get_account_summaries"}).then(function(data){
+                $scope.config.account_summaries_list = data['account_summaries'];
             });
-            
         });
-        
-        
     };
     
     
